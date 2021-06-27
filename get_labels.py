cfd9eb9ffb5e209322962a096f42686ab3348e5c
yuchengli <yuchengli@tencent.com>
2021-06-27 23:42:51 +0800

    venus 06.25

--- get_labels.py
+++ get_labels.py
@@ -3,4 +3,4 @@ n_lines = 81933324
 path = 'parsed_4/'
 dump_path = '/cfs/cfs-daenbe5b/latest-all.json.bz2'
 
-query_wikidata_dump_with_multi_processing(dump_path, path, n_lines, collect_labels=True, multi_lingual=True, skip_lines=48000000, num_procs=2)
+query_wikidata_dump_with_multi_processing(dump_path, path, n_lines, collect_labels=True, multi_lingual=True, skip_lines=54000000, num_procs=16, memory_lines=6000000)
